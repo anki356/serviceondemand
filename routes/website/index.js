c340fcc4ed42932692b4542fdea660c2f3a054a5
Ankita <anki356@gmil.com>
2024-11-14 08:41:18 +0530

    Website Service Route Added

--- routes/website/index.js
+++ routes/website/index.js
@@ -2,10 +2,10 @@ import express from 'express'
 const router=express.Router()
 // import AuthRouter from "./auth.js"
 import Dashboard from "./dashboard.js"
-// import ServiceRouter from "./service.js"
+import ServiceRouter from "./service.js"
 // import ProfileRouter from "./Profile.js"
 // router.use("/app/",AuthRouter)
 router.use("/website/",Dashboard)
-// router.use("/app/",ServiceRouter)
+router.use("/website/",ServiceRouter)
 // router.use("/app/",ProfileRouter)
 export default router
